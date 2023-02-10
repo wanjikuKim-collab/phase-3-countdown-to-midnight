1a15b17728f9ffd6fcbe345557315cb6b0f76c92
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-02-10 13:31:00 +0300

    completed countdown

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
+require 'pry'
 
-def countdown
+def countdown(i)    
+    while i>=1  
+        puts "#{i} SECOND(S)!"  
+        i-=1          
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(t)  
+    countdown(10)
+    sleep t  
+end
+
+countdown_with_sleep(5)
+
